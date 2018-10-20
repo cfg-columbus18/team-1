225cb1be3730d86ee502ef9a2ebed10f9358cb3a
zhicodes <zhide37@gmail.com>
2018-10-20 03:49:32 -0400

    asda
    asda

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
+const 
 
 /**
  * API keys and Passport configuration.
@@ -142,7 +143,6 @@ app.post('/account/profile', passportConfig.isAuthenticated, userController.post
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
-app.get('/mentors/mentor');
 
 /**
  * API examples routes.
