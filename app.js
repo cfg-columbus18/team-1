8262fd5c0b2a156d85a7da6faa923aac787d2441
Joe Huang <jch2220@columbia.edu>
2018-10-20 08:30:00 -0400

    Fix

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
   store: new MongoStore({
-    url: '52.90.61.82',
+    url: process.env.MONGODB_URI,
     autoReconnect: true,
   })
 }));
